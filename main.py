18be87903f4c2c6d3747fc6267a3522063f98058
Samxus <56377161+Samxus@users.noreply.github.com>
2021-08-08 20:22:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,82 +1,142 @@
-import torch
-import torch.nn as nn
-from torch.autograd import Variable
-from torchvision.utils import save_image
-from tqdm import tqdm
-import time
-from model import netD, netG
-from config import opt
-from Myutils import dataloader
+import os
+import ipdb
+import torch as t
+import torchvision as tv
+import tqdm
+from model import NetG, NetD
+from torchnet.meter import AverageValueMeter
 
-netd = netD(opt)
-netg = netG(opt)
 
-ds, dl = dataloader()
+class Config(object):
+    data_path = 'data/'  # 数据集存放路径
+    num_workers = 4  # 多进程加载数据所用的进程数
+    image_size = 96  # 图片尺寸
+    batch_size = 256
+    max_epoch = 400
+    lr1 = 2e-4  # 生成器的学习率
+    lr2 = 2e-4  # 判别器的学习率
+    beta1 = 0.5  # Adam优化器的beta1参数
+    gpu = True  # 是否使用GPU
+    nz = 100  # 噪声维度
+    ngf = 64  # 生成器feature map数
+    ndf = 64  # 判别器feature map数
 
-device = torch.device('cuda:0') if opt.use_gpu else torch.device('cpu')
-if opt.d_save_path:
-    netd.load_state_dict(torch.load(opt.d_save_path))
-    print("net_D loads weight successfully......")
-    print('___' * 10)
-if opt.g_save_path:
-    netg.load_state_dict(torch.load(opt.g_save_path))
-    print("net_G loads weight successfully......")
-    print('___' * 10)
-netd.to(device)
-netg.to(device)
-optm_g = torch.optim.Adam(netg.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-optm_d = torch.optim.Adam(netd.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-criterion = nn.BCELoss().to(device)
+    save_path = 'imgs/'  # 生成图片保存路径
 
-true_labels = Variable(torch.ones(opt.batch_size)).to(device)
-fake_labels = Variable(torch.zeros(opt.batch_size)).to(device)
-fix_noise = Variable(torch.randn(opt.batch_size, opt.noisy_dim, 1, 1))
-noises = Variable(torch.randn(opt.batch_size, opt.noisy_dim, 1, 1))
+    vis = True  # 是否使用visdom可视化
+    env = 'GAN'  # visdom的env
+    plot_every = 20  # 每间隔20 batch，visdom画图一次
 
-fix_noise, noises = fix_noise.to(device), noises.to(device)
+    debug_file = '/tmp/debuggan'  # 存在该文件则进入debug模式
+    d_every = 1  # 每1个batch训练一次判别器
+    g_every = 5  # 每5个batch训练一次生成器
+    save_every = 10  # 每10个epoch保存一次模型
+    netd_path = 'checkpoints/netd_399.pth'  
+    netg_path = 'checkpoints/netg_399.pth'  
 
-epochs = range(opt.max_epoch)
 
-for epoch in epochs:
-    with tqdm(enumerate(dl)) as t:
-        for ii, (img, _) in t:
+opt = Config()
+
+
+def train(**kwargs):
+    for k_, v_ in kwargs.items():
+        setattr(opt, k_, v_)
+
+    device=t.device('cuda') if opt.gpu else t.device('cpu')
+    if opt.vis:
+        from visualize import Visualizer
+        vis = Visualizer(opt.env)
+
+    # 数据
+    transforms = tv.transforms.Compose([
+        tv.transforms.Resize(opt.image_size),
+        tv.transforms.CenterCrop(opt.image_size),
+        tv.transforms.ToTensor(),
+        tv.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    dataset = tv.datasets.ImageFolder(opt.data_path, transform=transforms)
+    dataloader = t.utils.data.DataLoader(dataset,
+                                         batch_size=opt.batch_size,
+                                         shuffle=True,
+                                         num_workers=opt.num_workers,
+                                         drop_last=True
+                                         )
+
+    # 网络
+    netg, netd = NetG(opt), NetD(opt)
+    map_location = lambda storage, loc: storage
+    if opt.netd_path:
+        netd.load_state_dict(t.load(opt.netd_path, map_location=map_location))
+    if opt.netg_path:
+        netg.load_state_dict(t.load(opt.netg_path, map_location=map_location))
+    netd.to(device)
+    netg.to(device)
+
+
+    # 定义优化器和损失
+    optimizer_g = t.optim.Adam(netg.parameters(), opt.lr1, betas=(opt.beta1, 0.999))
+    optimizer_d = t.optim.Adam(netd.parameters(), opt.lr2, betas=(opt.beta1, 0.999))
+    criterion = t.nn.BCELoss().to(device)
+
+    # 真图片label为1，假图片label为0
+    # noises为生成网络的输入
+    true_labels = t.ones(opt.batch_size).to(device)
+    fake_labels = t.zeros(opt.batch_size).to(device)
+    fix_noises = t.randn(opt.batch_size, opt.nz, 1, 1).to(device)
+    noises = t.randn(opt.batch_size, opt.nz, 1, 1).to(device)
+
+    errord_meter = AverageValueMeter()
+    errorg_meter = AverageValueMeter()
+
+
+    epochs = tqdm.tqdm(range(opt.max_epoch))
+    for epoch in epochs:
+        for ii, (img, _) in enumerate(dataloader):
             real_img = img.to(device)
-            if ii % opt.d_step == 0:
-                optm_d.zero_grad()
-                real_outputs = netd(real_img)
-                error_d_real = criterion(real_outputs, true_labels)
+
+            if ii % opt.d_every == 0:
+                # 训练判别器
+                optimizer_d.zero_grad()
+                ## 尽可能的把真图片判别为正确
+                output = netd(real_img)
+                error_d_real = criterion(output, true_labels)
                 error_d_real.backward()
 
-                noises.data.copy_(torch.randn(opt.batch_size, opt.noisy_dim, 1, 1))
-                fake_img = netg(noises).detach()
-                fake_outputs = netd(fake_img)
-                error_d_fake = criterion(fake_outputs, fake_labels)
+                ## 尽可能把假图片判别为错误
+                noises.data.copy_(t.randn(opt.batch_size, opt.nz, 1, 1))
+                fake_img = netg(noises).detach()  # 根据噪声生成假图
+                output = netd(fake_img)
+                error_d_fake = criterion(output, fake_labels)
                 error_d_fake.backward()
-
-                optm_d.step()
+                optimizer_d.step()
 
                 error_d = error_d_fake + error_d_real
-                t.set_description("Epoch %s __ Batch %s" % (epoch, ii))
-                t.set_postfix(loss=error_d)
-                time.sleep(0.1)
 
-            if ii % opt.g_step == 0:
-                optm_g.zero_grad()
-                noises.data.copy_(torch.randn(opt.batch_size, opt.noisy_dim, 1, 1))
+                errord_meter.add(error_d.item())
+
+            if ii % opt.g_every == 0:
+                # 训练生成器
+                optimizer_g.zero_grad()
+                noises.data.copy_(t.randn(opt.batch_size, opt.nz, 1, 1))
                 fake_img = netg(noises)
-                outputs = netd(fake_img)
-                error_g = criterion(outputs, true_labels)
+                output = netd(fake_img)
+                error_g = criterion(output, true_labels)
                 error_g.backward()
-                optm_g.step()
-
-                t.set_description("Epoch %s __ Batch %s" % (epoch, ii))
-                t.set_postfix(loss=error_g)
-                time.sleep(0.1)
-
-        if (epoch + 1) % opt.save_every == 0:
-            g_img = netg(fix_noise)
-            save_image(g_img, '%s/%s_4.png' % (opt.save_path, epoch), normalize=True, range=(-1, 1))
-            torch.save(netg.state_dict(), 'checkpoints/netg_epoch_improved.pth')
-            torch.save(netd.state_dict(), 'checkpoints/netd_epoch_improved.pth')
-
-            print('Epoch: %s, NET_G_Error: %s' % (epoch, error_g.data.item()))
+                optimizer_g.step()
+                errorg_meter.add(error_g.item())
+                
+        epochs.set_postfix_str('error_g: {}, error_d: {}'.format(errorg_meter.mean, errord_meter.mean))
+        
+        if (epoch+1) % opt.save_every == 0:
+            # 保存模型、图片
+            fix_fake_imgs = netg(fix_noises)
+            tv.utils.save_image(fix_fake_imgs.data[:64], '%s/%s.png' % (opt.save_path, epoch), normalize=True,
+                                range=(-1, 1))
+            t.save(netd.state_dict(), 'checkpoints/netd_%s.pth' % epoch)
+            t.save(netg.state_dict(), 'checkpoints/netg_%s.pth' % epoch)
+            errord_meter.reset()
+            errorg_meter.reset()
+            
+ if __name__ == '__main__':
+    train()
